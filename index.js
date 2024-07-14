4924287f7308f443db55855763b91785cf70294c
shaan kodikara <119672480+Shaankodikara@users.noreply.github.com>
2024-07-14 22:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,16 @@ import { authRouter } from "./routes/auth.js";
 import { imageRouter } from "./routes/image.js";
 
 const app = express();
-app.use(cors());
-app.options('*', cors())
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("Connected to db....");
-      console.log("Server started and listening on port: " + process.env.PORT);
-    });
-  })
-  .catch((error) => console.log(error));
 
 // middlewares
 app.use(express.json());
 // app.use(express.static('./public'));
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use((req, res, next) => {
   console.log(req.method, req.path);
@@ -34,6 +29,16 @@ app.use((req, res, next) => {
 //routes
 app.use("/auth", authRouter);
 app.use("/image", imageRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db....");
+      console.log("Server started and listening on port: " + process.env.PORT);
+    });
+  })
+  .catch((error) => console.log(error));
 
 /* app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
