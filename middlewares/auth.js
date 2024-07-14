cc4cca79f9ba16c8750b6b9f7aacc77d4a0d0b79
shaan kodikara <119672480+Shaankodikara@users.noreply.github.com>
2024-07-14 22:47:15 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,10 @@ export const authorizeUser = async (req, res, next) => {
     try {
         const token = req.cookies.token
 
-        if(!token){
-            return res.json({status: "failed", message: "no token"})
-        }
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+        //if(!token){
+            //return res.json({status: "failed", message: "no token"})
+        //}
+        //const decoded = await jwt.verify(token, process.env.JWT_SECRET)
         next()
     } catch (error) {
        return res.json({error: error})
